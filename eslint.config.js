a3fb761c2bf7e9d24bdf53d06092ed86d3ad8508
Roid38911 <shiren0811@gmail.com>
2024-10-25 14:10:41 +0900

    second commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,39 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    // 設定の基本構造
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: "18.3", // 使用しているReactのバージョンを自動検出
+      },
+    },
+    plugins: {
+      js: pluginJs,
+      react: pluginReact,
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
